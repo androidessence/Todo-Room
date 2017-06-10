513897f2bcbabdfc6a8f7e32172a2b38d3fe259e
AdamMc331 <amcneilly331@gmail.com>
2017-06-10 18:37:23 -0400

    Removed fragment.

--- app/src/main/java/com/androidessence/todo_room/TaskActivity.kt
+++ app/src/main/java/com/androidessence/todo_room/TaskActivity.kt
@@ -3,7 +3,11 @@ package com.androidessence.todo_room
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class TaskActivity : AppCompatActivity() {
 
@@ -14,8 +18,16 @@ class TaskActivity : AppCompatActivity() {
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val fragment = TaskFragment()
-        supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        val recyclerview = findViewById(R.id.task_list) as RecyclerView
+        val layoutManager = LinearLayoutManager(this)
+        val adapter = TaskAdapter()
+        recyclerview.adapter = adapter
+        recyclerview.layoutManager = layoutManager
+
+        taskDao().getAll()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ adapter.tasks = it })
 
         val fab = findViewById(R.id.fab)
         fab.setOnClickListener {
