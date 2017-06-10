db656e817b746ccfe241aed3d3f0f073376b8afb
AdamMc331 <amcneilly331@gmail.com>
2017-06-09 22:31:55 -0400

    Initial commit.

---
+++ app/src/main/java/com/androidessence/todo_room/TaskActivity.kt
@@ -0,0 +1,26 @@
+package com.androidessence.todo_room
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+
+class TaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
